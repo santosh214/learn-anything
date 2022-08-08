6f55ffc6b32e5fbba3a7905d5c6692f0c8f24853
santosh214 <santoshsingh74827@gmail.com>
2022-08-08 12:32:38 +0530

    start

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import logo from '../logo.svg';
+
+export default function Header() {
+  return (
+    <>
+      <span className="App-header">
+        <img src={logo} className="App-logo" alt="logo" align="center" />
+      </span>
+    </>
+  );
+}
